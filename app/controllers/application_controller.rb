85f408854a8b4e99967e9ed6278f8d4d1c0b5f04
Did Youn <didier.youn@gmail.com>
2017-05-25 00:29:27 +0200

    #master : force to add locale in url (like mod_rewrite in htaccess) in order to prefix all the routes by /fr or /en

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,11 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   # Middleware for locale
-  before_action :set_locale
+  before_filter :set_locale
   
   # Set locale
   def set_locale
     I18n.locale = params[:locale] || I18n.default_locale
+    Rails.application.routes.default_url_options[:locale]= I18n.locale
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,21 +1,32 @@
 Rails.application.routes.draw do
 
-  # Admin routes
+  # i18n routes : [EN | FR]
   #
   # - Prefix : /locale => :fr | :en
-  scope "(:locale)", locale: /fr|en/ do
-    root to: 'frontend/index#index'
+  scope "/:locale", locale: /#{I18n.available_locales.join("|")}/ do
+    # Root URL
+    root to: "frontend/index#index"
 
+    # Routes defined for admin
     mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
-    # Front routes
+
+    # Routes defined for devise
     devise_for :users
+
+    # GraphQL POST queries
+    post "/graphql", to: "frontend/graphql#query"
+
+    # Development environment
+    if Rails.env.development?
+      mount GraphiQL::Rails::Engine, at: "/graphiql", graphql_path: "/graphql"
+    end
   end
 
-  # GraphQL POST queries
-  post '/graphql', to: 'frontend/graphql#query'
-
-  # Development environment
-  if Rails.env.development?
-    mount GraphiQL::Rails::Engine, at: "/graphiql", graphql_path: "/graphql"
-  end
+  # Simulate MOD_REWRITE HTACCESS
+  # Example : [ / ] will return [ /fr ]
+  # Flag : 302 as permanent redirection
+  root to: redirect("/#{I18n.default_locale}", status: 302), as: :redirected_root
+  get "/*path", to: redirect("/#{I18n.default_locale}/%{path}", status: 302), constraints: {path: /(?!(#{I18n.available_locales.join("|")})\/).*/}, format: false
 end
+
+
