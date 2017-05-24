abbcdd9195b2efb47549d23de5524bade51e44cd
Did Youn <didier.youn@gmail.com>
2017-05-25 00:42:47 +0200

    #admin :create admin

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   # Middleware for locale
   before_filter :set_locale
-  
+
   # Set locale
   def set_locale
     I18n.locale = params[:locale] || I18n.default_locale
