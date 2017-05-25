24c8e5a7656ee9773cf077dede9759ec5b860847
Did Youn <didier.youn@gmail.com>
2017-05-25 22:28:42 +0200

    #admin : add devise templates, override devise controller and add scss script

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   # Middleware for locale
-  before_filter :set_locale
+  before_action :set_locale
 
   # Set locale
   def set_locale
