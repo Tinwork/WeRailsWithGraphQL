09894501533917f20d5a5d21d8cbec97effff52f
Did Youn <didier.youn@gmail.com>
2017-06-07 23:04:25 +0200

    #admin_evolutions : add seeds

---
+++ db/seeds/category.rb
@@ -0,0 +1,30 @@
+c1 = Category.create({
+  label: 'Légume vert'
+})
+c2 = Category.create({
+  label: 'Viande rouge'
+})
+c3 = Category.create({
+  label: 'Viande blanche'
+})
+c4 = Category.create({
+  label: 'Pain'
+})
+c5 = Category.create({
+   label: 'Fromage'
+})
+c6 = Category.create({
+  label: 'Soda'
+})
+c7 = Category.create({
+  label: 'Jus de fruit'
+})
+c8 = Category.create({
+  label: 'Boisson chaude'
+})
+c9 = Category.create({
+  label: 'Féculents'
+})
+c10 = Category.create({
+  label: 'Fruits'
+})
---
+++ db/seeds/ingredients.rb
@@ -0,0 +1,47 @@
+require 'category'
+
+i1 = Ingredient.create({
+  label: 'Salade',
+  calories: 100.50,
+  category: c1
+})
+i2 = Ingredient.create({
+   label: 'Tomate',
+   calories: 100.50,
+   category: c1
+})
+i3 = Ingredient.create({
+  label: 'Oignon',
+  calories: 100.50,
+  category: c1
+})
+i4 = Ingredient.create({
+   label: 'Steak Haché',
+   calories: 100.50,
+   category: c2
+})
+i5 = Ingredient.create({
+  label: 'Poulet',
+  calories: 100.50,
+  category: c3
+})
+i6 = Ingredient.create({
+  label: 'Pain',
+  calories: 100.50,
+  category: c4
+})
+i7 = Ingredient.create({
+   label: 'Cheedar',
+   calories: 100.50,
+   category: c5
+})
+i8 = Ingredient.create({
+  label: 'Roquefort',
+  calories: 100.50,
+  category: c5
+})
+i8 = Ingredient.create({
+  label: 'Bacon',
+  calories: 100.50,
+  category: c2
+})
