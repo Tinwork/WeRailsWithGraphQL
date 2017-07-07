5a641b471020921a6e42c17a3bf04cdec687975d
Did Youn <didier.youn@gmail.com>
2017-07-07 23:47:26 +0200

    #admin_evolutions : fix scss, alert messages

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,7 +12,7 @@ class User < ApplicationRecord
   attr_accessor :login
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable,
+  devise :database_authenticatable,
          :recoverable, :rememberable, :trackable, :validatable,
          :authentication_keys => [:login]
   # Validations
